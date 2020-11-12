add7ca4a5f97b7d373671d592ebb4b68b23d026c
attoiumani <attoiumani0313@gmail.com>
2020-11-12 15:28:09 +0900

    Update index.php

--- theme/index.php
+++ theme/index.php
@@ -11,7 +11,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>ここにタイトル</title>
+    <title>ここにiタイトル</title>
 
     <link rel="icon" href="favicon.ico">
     <link rel="stylesheet" href="<?php  echo get_template_directory_uri(); ?>/style.css">
