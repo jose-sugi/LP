bf6e3afb6641d7bfd10f3c2e43efac5af65d5a5d
shirakawa <attoiumani0313@gmail.com>
2020-11-16 18:25:46 +0900

    Update index.php

--- theme/index.php
+++ theme/index.php
@@ -11,7 +11,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>シュウセイ</title>
+    <title>修正</title>
 
     <link rel="icon" href="favicon.ico">
     <link rel="stylesheet" href="<?php  echo get_template_directory_uri(); ?>/style.css">
