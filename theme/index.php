df85118d827dad6d75018de4947493deb2a85221
jose-sugi <h.s.miruku.1121@gmail.com>
2020-11-16 18:09:07 +0900

    Update index.php

--- theme/index.php
+++ theme/index.php
@@ -11,7 +11,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>ここにタイトル　杉澤</title>
+    <title>ここにタイトル</title>
 
     <link rel="icon" href="favicon.ico">
     <link rel="stylesheet" href="<?php  echo get_template_directory_uri(); ?>/style.css">
